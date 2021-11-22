ba0627ff2c057fef6a0758ea1eb9818eedf0f056
Katrina Forchuk <katrinaforchuk@gmail.com>
2021-11-22 13:22:25 -0700

    finish navbar element

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import Counter from "./Counter";
+import Counter from "./counter";
 
 class Counters extends Component{
     render(){
@@ -38,3 +38,5 @@ class Counters extends Component{
         );
     }
 }
+
+export default Counters;
